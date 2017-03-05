9969f18ba1245425a1d5fa3b81c93f47313649b2
Radomir <radomir.mastalerz@wealtharc.com>
2017-03-05 17:55:11 +0100

    all working

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,19 +1,27 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {LatestVideoService} from "./latest-video.service";
-//import { Video } from '../video.service.ts';
+import {SortingService} from "./sorting.service";
 
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
-  styleUrls: ['./video-list.component.css'],
-  providers: [LatestVideoService]
+  styleUrls: ['./video-list.component.css', '../app.component.css'],
+  providers: [SortingService]
 })
 export class VideoListComponent implements OnInit {
   @Input() videos: any[] = [];
 
-  constructor() {
+  constructor(private sortingService: SortingService) {
   }
 
   ngOnInit() {
+  }
+
+  sortAscending() {
+    this.videos = this.videos.sort(this.sortingService.ascending);
+  }
+
+  sortDescending() {
+    this.videos = this.videos.sort(this.sortingService.descending);
   }
 }
