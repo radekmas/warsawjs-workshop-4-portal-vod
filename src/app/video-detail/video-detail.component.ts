bc2980858d8aff749c01a4685d9bd70dd09fb0fa
Radomir <radomir.mastalerz@wealtharc.com>
2017-03-05 13:02:08 +0100

    app-routing module

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-video-detail',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-detail.component.css']
 })
 export class VideoDetailComponent implements OnInit {
-
+  @Input() video: any;
   constructor() { }
 
   ngOnInit() {
